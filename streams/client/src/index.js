dfb111e93f6dad90eaa860f1c82aa8519df62c84
mrajewski <rajewski.ma@gmail.com>
2020-04-23 15:33:23 +0200

    created json-server and ajax post for new stream

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
@@ -7,10 +8,7 @@ import reducers from "./reducers"
 import App from './components/App'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    reducers,
-    composeEnhancers(applyMiddleware())
-);
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
