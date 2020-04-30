5a2c5188078a6da6928e9c5e0d5940b04771283d
mrajewski <rajewski.ma@gmail.com>
2020-04-30 15:24:37 +0200

    stream list and navigation

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
     DELETE_STREAM
 } from "./types";
 import streams from "../apis/streams";
+import history from "../history";
 
 export const signIn = (userId) => {
     return {
@@ -22,10 +23,14 @@ export const signOut = () => {
     }
 };
 
-export const createStream = formValues => async dispatch => {
-    const response = await streams.post("/streams", formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post("/streams", {...formValues, userId});
 
     dispatch({type: CREATE_STREAM, payload: response.data})
+
+    // Programmatic navigation to back to the root route
+    history.push('/')
 };
 
 export const fetchStreams = () => async dispatch => {
